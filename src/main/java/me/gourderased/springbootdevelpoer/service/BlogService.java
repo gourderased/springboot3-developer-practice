a0c3eb25ac242d9e618b4fd5c8c1fafe80da4b9c
gourderased <gourderased@gmail.com>
2023-07-05 16:32:23 +0900

    ch.6

--- src/main/java/me/gourderased/springbootdevelpoer/service/BlogService.java
+++ src/main/java/me/gourderased/springbootdevelpoer/service/BlogService.java
@@ -1,10 +1,14 @@
 package me.gourderased.springbootdevelpoer.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.gourderased.springbootdevelpoer.domain.Article;
 import me.gourderased.springbootdevelpoer.dto.AddArticleRequest;
+import me.gourderased.springbootdevelpoer.dto.UpdateArticleRequest;
 import me.gourderased.springbootdevelpoer.reository.BlogRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +18,28 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
