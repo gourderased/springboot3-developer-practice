f006e164c1f71d4b42a37fc6cbe43e7cf445661f
gourderased <gourderased@gmail.com>
2023-07-30 18:24:40 +0900

    ch.7

--- src/main/java/me/gourderased/springbootdevelpoer/domain/Article.java
+++ src/main/java/me/gourderased/springbootdevelpoer/domain/Article.java
@@ -1,9 +1,12 @@
 package me.gourderased.springbootdevelpoer.domain;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Getter
@@ -31,4 +34,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name ="created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
---
+++ src/main/java/me/gourderased/springbootdevelpoer/dto/ArticleViewResponse.java
@@ -0,0 +1,22 @@
+package me.gourderased.springbootdevelpoer.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.gourderased.springbootdevelpoer.domain.Article;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
